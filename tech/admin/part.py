a5ae367dcb61a5a1cbafee6ebc1c836c3a872462
dhefley <dhefley@meridianne.com>
2022-02-26 08:29:53 -0600

    style admin photos

--- tech/admin/part.py
+++ tech/admin/part.py
@@ -11,7 +11,7 @@ class PartAdmin(admin.ModelAdmin):
     fmtCost.short_description="Cost"
 
     def picture(self, obj):
-        return SafeString("<img src='{}' height='100px' />".format( obj.image.url))
+        return SafeString("<div style='background-image: url({});width: 100px;height: 100px;background-position: center center;background-size: cover;background-repeat: no-repeat;border-radius: 50%;'></div>".format( obj.image.url))
 
     list_display = ("picture","name", "fmtCost", "status",)
     list_display_links = ("picture", "name",)
